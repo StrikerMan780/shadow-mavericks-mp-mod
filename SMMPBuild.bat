4319db19bdcba77e49e9253577cabbdae8f5b329
Jordon Moss <mossj32@gmail.com>
2015-03-20 23:19:21 -0300

    Committed new GIT Compiler for SMMP
    Needed a new GIT-based compiler, since SVN is no longer being used.

--- SMMPBuild.bat
+++ SMMPBuild.bat
@@ -4,9 +4,9 @@ set WorkingCopyPath=%~dp0
 cls
 
 echo ------------------------------
-echo Retrieving SVN Revision Number
+echo Retrieving GIT Revision Number
 echo ------------------------------
-for /f "tokens=5" %%i in ('"SubWCRev "%WorkingCopyPath%.""') do set REVISIONNUMBER=%%i
+for /f "delims=" %%i in ('git rev-list HEAD --count') do set REVISIONNUMBER=%%i
 
 :MENU
 cd /d %~dp0
@@ -21,7 +21,7 @@ chgcolor 0a
 echoj $ba $0a
 echoj $ba
 chgcolor 0f
-echoj " Development and SVN Build Compiler "
+echoj " Development and GIT Build Compiler "
 chgcolor 0a
 echoj $ba $0a
 echo 藩様様様様様様様様様様様様様様様様様夕
@@ -32,7 +32,7 @@ echoj "Current Path Is: "
 chgcolor 0b
 echo %~dp0
 chgcolor 07
-echoj "Current SVN Revision: "
+echoj "Current GIT Revision: "
 chgcolor 0c
 echo %REVISIONNUMBER%
 echo.
@@ -49,13 +49,13 @@ echo.
 chgcolor 0f
 echoj "3. "
 chgcolor 0e
-echoj "Build SVN Release Core Module "
+echoj "Build GIT Release Core Module "
 chgcolor 0a
 echoj "(FULL)" $0a
 chgcolor 0f
 echoj "4. "
 chgcolor 06
-echoj "Build SVN Release Extras Module "
+echoj "Build GIT Release Extras Module "
 chgcolor 0a
 echoj "(FULL)" $0a
 
@@ -63,13 +63,13 @@ echo.
 chgcolor 0f
 echoj "5. "
 chgcolor 0e
-echoj "Build SVN Release Core Module "
+echoj "Build GIT Release Core Module "
 chgcolor 0d
 echoj "(QUICK)" $0a
 chgcolor 0f
 echoj "6. "
 chgcolor 06
-echoj "Build SVN Release Extras Module "
+echoj "Build GIT Release Extras Module "
 chgcolor 0d
 echoj "(QUICK)" $0a
 echo.
@@ -82,10 +82,10 @@ chgcolor 07
 echo.
 CHOICE /C 1234567 /N /M "Choose Option (Number Keys):"
 IF ERRORLEVEL 7 GOTO LEAVE
-IF ERRORLEVEL 6 GOTO SVNEXTRASQUICK
-IF ERRORLEVEL 5 GOTO SVNCOREQUICK
-IF ERRORLEVEL 4 GOTO SVNEXTRASFULL
-IF ERRORLEVEL 3 GOTO SVNCOREFULL
+IF ERRORLEVEL 6 GOTO GITEXTRASQUICK
+IF ERRORLEVEL 5 GOTO GITCOREQUICK
+IF ERRORLEVEL 4 GOTO GITEXTRASFULL
+IF ERRORLEVEL 3 GOTO GITCOREFULL
 IF ERRORLEVEL 2 GOTO DEVEXTRAS
 IF ERRORLEVEL 1 GOTO DEVCORE
 
@@ -94,7 +94,7 @@ echo Compiling SMMP Development Core Module...
 del .\builds\SMMP_Core_DEV.pk3 /q
 
 cd pk3
-7za a -y -tzip -mx=0 -mmt -x!.svn ..\builds\SMMP_Core_DEV.pk3 .\
+7za a -y -tzip -mx=0 -mmt -x!.GIT ..\builds\SMMP_Core_DEV.pk3 .\
 
 pause
 goto MENU
@@ -107,53 +107,53 @@ move /Y .\pk3_extras\Maps\*.dbs .\backups >nul 2>&1
 move /Y .\pk3_extras\Maps\*.bak .\backups >nul 2>&1
 
 cd pk3_extras
-7za a -y -tzip -mx=0 -mmt -x!.svn ..\builds\SMMP_MapsAndExtras_DEV.pk3 .\
+7za a -y -tzip -mx=0 -mmt -x!.GIT ..\builds\SMMP_MapsAndExtras_DEV.pk3 .\
 
 pause
 goto MENU
 
-:SVNCOREFULL
-echo Compiling SMMP SVN Core Module Rev#: %REVISIONNUMBER% (Full Compression)...
+:GITCOREFULL
+echo Compiling SMMP GIT Core Module Rev#: %REVISIONNUMBER% (Full Compression)...
 del .\builds\SMMP_Core_r%REVISIONNUMBER%.pk3 /q
 
 cd pk3
-7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\SMMP_Core_r%REVISIONNUMBER%.pk3 .\
+7za a -y -tzip -mx=9 -mmt -x!.GIT ..\builds\SMMP_Core_r%REVISIONNUMBER%.pk3 .\
 
 pause
 goto MENU
 
-:SVNEXTRASFULL
-echo Compiling SMMP SVN Extras Module Rev#: %REVISIONNUMBER% (Full Compression)...
+:GITEXTRASFULL
+echo Compiling SMMP GIT Extras Module Rev#: %REVISIONNUMBER% (Full Compression)...
 del .\builds\SMMP_MapsAndExtras_r%REVISIONNUMBER%.pk3 /q
 move /Y .\pk3_extras\Maps\*.backup* .\backups >nul 2>&1
 move /Y .\pk3_extras\Maps\*.dbs .\backups >nul 2>&1
 move /Y .\pk3_extras\Maps\*.bak .\backups >nul 2>&1
 
 cd pk3_extras
-7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\SMMP_MapsAndExtras_r%REVISIONNUMBER%.pk3 .\
+7za a -y -tzip -mx=9 -mmt -x!.GIT ..\builds\SMMP_MapsAndExtras_r%REVISIONNUMBER%.pk3 .\
 
 pause
 goto MENU
 
-:SVNCOREQUICK
-echo Compiling SMMP SVN Core Module Rev#: %REVISIONNUMBER% (No Compression)...
+:GITCOREQUICK
+echo Compiling SMMP GIT Core Module Rev#: %REVISIONNUMBER% (No Compression)...
 del .\builds\SMMP_Core_r%REVISIONNUMBER%.pk3 /q
 
 cd pk3
-7za a -y -tzip -mx=0 -mmt -x!.svn ..\builds\SMMP_Core_r%REVISIONNUMBER%.pk3 .\
+7za a -y -tzip -mx=0 -mmt -x!.GIT ..\builds\SMMP_Core_r%REVISIONNUMBER%.pk3 .\
 
 pause
 goto MENU
 
-:SVNEXTRASQUICK
-echo Compiling SMMP SVN Extras Module Rev#: %REVISIONNUMBER% (No Compression)...
+:GITEXTRASQUICK
+echo Compiling SMMP GIT Extras Module Rev#: %REVISIONNUMBER% (No Compression)...
 del .\builds\SMMP_MapsAndExtras_r%REVISIONNUMBER%.pk3 /q
 move /Y .\pk3_extras\Maps\*.backup* .\backups >nul 2>&1
 move /Y .\pk3_extras\Maps\*.dbs .\backups >nul 2>&1
 move /Y .\pk3_extras\Maps\*.bak .\backups >nul 2>&1
 
 cd pk3_extras
-7za a -y -tzip -mx=0 -mmt -x!.svn ..\builds\SMMP_MapsAndExtras_r%REVISIONNUMBER%.pk3 .\
+7za a -y -tzip -mx=0 -mmt -x!.GIT ..\builds\SMMP_MapsAndExtras_r%REVISIONNUMBER%.pk3 .\
 
 pause
 goto MENU
